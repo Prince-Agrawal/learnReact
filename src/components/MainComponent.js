37b7fa1d4234bc01adafaa0ad605f338f98a4232
Prince-Agrawal <utkarshagrawal2022@gmail.com>
2020-08-05 15:14:32 +0530

    assignement-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,9 +4,15 @@ import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import Header from "./Header";
 import Footer from "./Footer";
+import Contact from "./ContactComponent";
+import DishDetail from './DishDetails';
+import About from './AboutComponent';
 import Home from "./HomeComponent";
 import { Component } from "react";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import DishDetails from "./DishDetails";
 import { Switch, Route, Redirect } from "react-router-dom";
 
@@ -15,31 +21,60 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
-  onDishSelect(dishID) {
-    this.setState({ selectedDish: dishID });
-  }
-
   render() {
-    function HomePage() {
-      return (
-        <div>
-          <Home />
-        </div>
-      );
+    // function HomePage() {
+    //   // console.log(this.state.dishes)
+    //   return (
+    //     <div>
+
+    //     </div>
+    //   );
+    // }
+
+    const HomePage = ()=>{
+      return(
+        <Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={
+                  this.state.promotions.filter((promo) => promo.featured)[0]
+                }
+                leader={
+                  this.state.leaders.filter((leader) => leader.featured)[0]
+                }
+              />
+      )
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return (
       <div>
         <Header />
         <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Redirect to="/home" />
-          </Switch>
+          <Route
+            path="/home"
+            component={HomePage}
+          />
+          <Route
+            exact
+            exact path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={()=> <About leaders = {this.state.leaders} />} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle , Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish, onClick }) {
   return (
-    <Card key={dish.id} onClick={() => onClick(dish.id)}>
-      <CardImg width="100%" src={dish.image} alt={dish.name} />
-      <CardImgOverlay>
-        <CardTitle>{dish.name}</CardTitle>
-      </CardImgOverlay>
+    <Card key={dish.id}>
+      <Link to={`/menu/${dish.id}`}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
     </Card>
   );
 }
@@ -16,15 +19,27 @@ const Menu = (props) => {
   const menu = props.dishes.map((dish) => {
     return (
       <div className="col-12 col-md-5 m-1" key={dish.id}>
-        <RenderDish dish={dish} onClick={props.onClick} />
+        <RenderDish dish={dish} />
       </div>
     );
   });
   return (
     <div className="container">
-      <div className="row">{menu}</div>
+        <div className="row">
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                <BreadcrumbItem active>Menu</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>Menu</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            {menu}
+        </div>
     </div>
-  );
+);
 };
 
 export default Menu;
