e61484623d9696f61f0ba751c2df90810794d566
Prince-Agrawal <utkarshagrawal2022@gmail.com>
2020-08-06 17:41:10 +0530

    controlled form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import Header from "./Header";
 import Footer from "./Footer";
